b689acd1c1f091ac1a6600c503695181a8b5143d
Shreyans <shreyanssaraf10@gmail.com>
2025-09-05 22:04:56 +0530

    Revert "Backend for hackathon"
    This reverts commit 57d03c902e2dbdcec1ef0d08dbdd58558b3cb9b2.

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,10 @@
 # Environment variables (DB_URI, JWT_SECRET)
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MONGO_URI: str
     AYUSHMITRA: str
-    GROQ1: str
-    GROQ2: str
-    GROQ3: str
-    DB_NAME: str
-    INGREDIENTS: str
-    RECIPES: str
-    ACCOUNTS: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
